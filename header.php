d9aa6ebafef72f20177765ea284deb7084ca9561
Yuta Takata <y.takata1125@gmail.com>
2019-01-12 22:51:56 +0900

    init

---
+++ header.php
@@ -0,0 +1,26 @@
+
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</a>
+			<div class="nav-collapse collapse">
+				<ul class="nav">
+					<li class="active"><a href="#">Hello</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Links <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="http://www.goto.info.waseda.ac.jp/index-j.html">Goto Laboratory</a></li>
+						<!--<li class="nav-header">Nav header</li>-->
+							<li><a href="http://www.sci.waseda.ac.jp/">早稲田大学 理工学術院</a></li>
+							<li><a href="http://www.waseda.jp/top/index-j.html">早稲田大学</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>高田雄太 (Yuta Takata) | 早稲田大学 後藤滋樹研究室</title>
+<meta name="description" content="高田雄太 (Yuta Takata) | 早稲田大学, Waseda University Fundamental Engineering Computer Science and Engineering, GOTO Laboratoty" />
+<meta name="keywords" content="Yuta Takata,高田雄太,Waseda University,Shigeki Goto,Network,Security,Web,Honeypot" />
+<meta name="author" content="Yuta Takata">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/bootstrap.css" rel="stylesheet">
+<style type="text/css">
+body { padding-top: 60px; padding-bottom: 40px; }
+a,a:visited {color: #41924B;}
+h2,h3 {color: #FE8402;}
+.me {text-decoration: underline;}
+</style>
+<link href="css/bootstrap-responsive.css" rel="stylesheet">
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="img/favicon.png">
+</head>
+<body>
+<?php require_once("header.php"); ?>
+<div class="container">
+	<div class="hero-unit" style="height:150px;"></div>
+	<div class="row">
+		<div class="span6">
+			<h2>About me</h2>
+			<p>
+				<ul>
+					<li><b>Yuta TAKATA</b></li>
+					<li>y.takata (a) goto.info.waseda.ac.jp</li>
+					<li>I received my M.E. and Ph.D. from Waseda University in 2013 and 2018, respectively.</li>
+					<li>I am a researcher at NTT R&D and a member of NTT-CERT. Since joining NTT in 2013, I have been engaged in R&D of technologies related to cyber security.</li>
+					<li>I am also an adjunct instructor at Waseda University since 2018.</li>
+				</ul>
+			</p>
+		</div>
+		<div class="span3">
+			<h2>Interests</h2>
+			<p>
+				<ul>
+					<li>Web Security</li>
+					<li>Cyber Security</li>
+					<li>Honeypot / Honeyclient</li>
+					<li>Program Analysis</li>
+				</ul>
+			</p>
+		</div>
+		<div class="span3">
+			<h2>Like</h2>
+			<p>
+				<ul>
+					<li>Swimming, Tennis, Snowboarding</li>
+					<li>Trip</li>
+					<li>Beer and Wine</li>
+				</ul>
+			</p>
+		</div>
+	</div>
+	<hr />
+	<div class="row">
+		<div class="span12">
+			<h3>Doctoral Thesis</h3>
+			<ul>
+				<li><b><a href="http://hdl.handle.net/2065/00057177" target="_blank">Analyzing Hidden Features of Web-based Attacks.</a></b><br/>
+					<span class="me">Yuta Takata</span><br />
+					Doctoral Thesis, March 2018.</li>
+			</ul>
+			<h3>Refereed Journal</h3>
+			<ul>
+				<li><b>Evasive Malicious Website Detection by Leveraging Redirection Subgraph Similarities.</b> <span class="label">To appear</span><br />
+					Toshiki Shibahara, <span class="me">Yuta Takata</span>, Mitsuaki Akiyama, Takeshi Yagi, Kunio Hato, and Masayuki Murata.<br />
+					IEICE TRANSACTIONS on Information and Systems, Vol.XX, No.XX, pp.XX--XX, March 2019.</li>
+				<li><b><a href="https://doi.org/10.1587/transfun.E101.A.2149" target="_blank">Event De-noising Convolutional Neural Network for Detection Malicious URL Sequences from Proxy Logs.</a></b> <a href="https://doi.org/10.1587/transfun.E101.A.2149" target="_blank"><span class="label label-info">DOI</span><br />
+					Toshiki Shibahara, Kohei Yamanishi, <span class="me">Yuta Takata</span>, Daiki Chiba, Taiga Hokaguchi, Mitsuaki Akiyama, Takeshi Yagi, Yuichi Ohsita, and Masayuki Murata.<br />
+					IEICE TRANSACTIONS on Fundamental of Electronics, Communications and Computer Sciences, Vol.E101--A, No.12, pp.2149--2161, December 2018.</li>
+				<li><b><a href="https://doi.org/10.1587/transinf.2017ICP0005" target="_blank">Identifying Evasive Code in Malicious Websites By Analyzing Redirection Differences.</a></b> <a href="https://doi.org/10.1587/transinf.2017ICP0005" target="_blank"><span class="label label-info">DOI (Open Access)</span></a><br />
+					<span class="me">Yuta Takata</span>, Mitsuaki Akiyama, Takeshi Yagi, Takeo Hariu, Kazuhiko Ohkubo, and Shigeki Goto.<br />
+					IEICE TRANSACTIONS on Information and Systems, Vol.E101--D, No.11, pp.2600--2611, November 2018.</li>
+				<li><b><a href="https://doi.org/10.1587/transinf.2017ICP0009" target="_blank">Towards Finding Code Snippets on a Question and Answer Website Causing Mobile App Vulnerabilities.</a></b> <a href="https://doi.org/10.1587/transinf.2017ICP0009" target="_blank"><span class="label label-info">DOI (Open Access)</span></a><br />
+					Hiroki Nakano, Fumihiro Kanei, <span class="me">Yuta Takata</span>, Mitsuaki Akiyama, and Katsunari Yoshioka.<br />
+					IEICE TRANSACTIONS on Information and Systems, Vol.E101--D, No.11, pp.2576--2583, November 2018.</li>
+				<li><b><a href="https://doi.org/10.1587/transinf.2016ICP0011" target="_blank">Fine-grained Analysis of Compromised Websites with Redirection Graphs and JavaScript Traces.</a></b> <a href="https://doi.org/10.1587/transinf.2016ICP0011" target="_blank"><span class="label label-info">DOI (Open Access)</span></a><br />
+					<span class="me">Yuta Takata</span>, Mitsuaki Akiyama, Takeshi Yagi, Takeshi Yada, and Shigeki Goto.<br />
+					IEICE TRANSACTIONS on Information and Systems, Vol.E100--D, No.8, pp.1714--1728, August 2017.</li>
+				<li><b><a href="https://doi.org/10.1587/transinf.2016ICP0020" target="_blank">Building a Scalable Web Tracking Detection System: Implementation and the Empirical Study.</a></b> <a href="https://doi.org/10.1587/transinf.2016ICP0020" target="_blank"><span class="label label-info">DOI (Open Access)</span></a><br />
+					Yumehisa Haga, <span class="me">Yuta Takata</span>, Mitsuaki Akiyama, and Tatsuya Mori.<br />
+					IEICE TRANSACTIONS on Information and Systems, Vol.E100--D, No.8, pp.1663--1670, August 2017.</li>
+				<li><b><a href="https://doi.org/10.1587/transinf.2015ICP0013" target="_blank">MineSpider: Extracting Hidden URLs Behind Evasive Drive-by Download Attacks.</a></b> <a href="https://doi.org/10.1587/transinf.2015ICP0013" target="_blank"><span class="label label-info">DOI (Open Access)</span></a><br />
+					<span class="me">Yuta Takata</span>, Mitsuaki Akiyama, Takeshi Yagi, Takeo Hariu, and Shigeki Goto.<br />
+					IEICE TRANSACTIONS on Information and Systems, Vol.E99--D, No.4, pp.860--872, April 2016.</li>
+			</ul>
+			<h3>Refereed Conference</h3>
+			<ul>
+				<li><b><a href="https://www.first.org/conference/2018/program#pdiscovering-evasive-code-in-malicious-websites-with-high-and-low-interaction-honeyclients" target="_blank">Discovering Evasive Code in Malicious Websites with High- and Low-interaction Honeyclients.</a></b>
+					<a href="https://www.first.org/conference/2018/program#pdiscovering-evasive-code-in-malicious-websites-with-high-and-low-interaction-honeyclients" target="_blank"><span class="label label-info">Abstract</span></a>
+					<a href="pdf/FIRST30-takata.pdf" target="_blank"><span class="label label-info">Slide</span></a>
+					<a href="https://blog.apnic.net/2018/08/07/discovering-evasive-code-in-malicious-websites/" target="_blank"><span class="label label-success">APNIC Blog</span></a>
+					<a href="https://www.theregister.co.uk/2018/08/09/how_evil_javascript_helps_attackers_tag_possible_victims_and_gives_away_their_intent/" target="_blank"><span class="label label-success">The Register</span></a>
+					<br />
+					<span class="me">Yuta Takata</span><br />
+					30th Annual FIRST Conference, June 2018.<br />
+					Acceptance rate: under 32.5% (65/over 200)</li>
+				<li><b><a href="https://doi.org/10.1007/978-3-319-68786-5_22" target="_blank">Understanding Evasion Techniques that Abuse Differences among JavaScript Implementations.</a></b> <a href="https://doi.org/10.1007/978-3-319-68786-5_22" target="_blank"><span class="label label-info">DOI</span></a><br />
+					<span class="me">Yuta Takata</span>, Mitsuaki Akiyama, Takeshi Yagi, Takeo Hariu, and Shigeki Goto.<br />
+					Proceedings of the 18th International Conference on Web Information Systems Engineering (WISE), October 2017.<br />
+					Acceptance rate: 25.0% (49/196)</li>
+				<li><b><a href="https://doi.org/10.1145/3121264.3121267" taget="_blank">Understanding the Security Management of Global Third-Party Android Marketplaces.</a></b> <a href="https://doi.org/10.1145/3121264.3121267" taget="_blank"><span class="label label-info">DOI</span></a><br />
+					Yuta Ishii, Takuya Watanabe, Fumihiko Kanei, <span class="me">Yuta Takata</span>, Eitaro Shioji, Mitsuaki Akiyama, Takeshi Yagi, Bo Sun, and Tatsuya Mori.<br />
+					Proceedings of the 2nd International Workshop on App Market Analytics (WAMA), September 2017.</li>
+				<li><b><a href="https://doi.org/10.1109/COMPSAC.2017.105" target="_blank">Detecting Malicious Websites by Integrating Malicious, Benign, and Compromised Redirection Subgraph Similarities.</a></b> <a href="https://doi.org/10.1109/COMPSAC.2017.105" target="_blank"><span class="label label-info">DOI</span></a><br />
+					Toshiki Shibahara, <span class="me">Yuta Takata</span>, Mitsuaki Akiyama, Takeshi Yagi, and Takeshi Yada.<br />
+					Proceedings of the 41th Annual International Computers, Software & Applications Conference (COMPSAC), July 2017.<br />
+					Acceptance rate: 20% (58/290)</li>
+				<li><b><a href="https://doi.org/10.1109/MSR.2017.23" target="_blank">Understanding the Origins of Mobile App Vulnerabilities: A Large-scale Measurement Study of Free and Paid Apps.</a></b> <a href="https://doi.org/10.1109/MSR.2017.23" target="_blank"><span class="label label-info">DOI</span></a> <a href="https://dl.acm.org/citation.cfm?id=3104193" target="_blank"><span class="label label-info">ACM</span></a><br />
+					Takuya Watanabe, Mitsuaki Akiyama, Fumihiro Kanei, Eitaro Shioji, <span class="me">Yuta Takata</span>, Bo Sun, Yuta Ishii, Toshiki Shibahara, Takeshi Yagi, and Tatsuya Mori.<br />
+					Proceedings of the 14th International Conference on Mining Software Repositories (MSR), May 2017.<br />
+					Acceptance rate: 30.6% (37/121)</li>
+				<li><b><a href="https://doi.org/10.1109/ICC.2017.7996831" target="_blank">Malicious URL Sequence Detection using Event De-noising Convolutional Neural Network.</a></b> <a href="https://doi.org/10.1109/ICC.2017.7996831" target="_blank"><span class="label label-info">DOI</span></a><br />
+					Toshiki Shibahara, Kohei Yamanishi, <span class="me">Yuta Takata</span>, Daiki Chiba, Mitsuaki Akiyama, Takeshi Yagi, Yuichi Ohsita, and Masayuki Murata.<br />
+					Proceedings of IEEE International Conference on Communications (ICC), May 2017.<br />
+					Acceptance rate of ICC-CISS: 30.5% (84/275)</li>
+				<li><b><a href="https://doi.org/10.1007/978-3-319-59608-2_25" target="_blank">Website Forensic Investigation to Identify Evidence and Impact of Compromise.</a></b> <a href="https://doi.org/10.1007/978-3-319-59608-2_25" target="_blank"><span class="label label-info">DOI</span></a> <a href="pdf/takata-SecureComm16.pdf" target="_blank"><span class="label label-info">preprint</span></a><br />
+					<span class="me">Yuta Takata</span>, Mitsuaki Akiyama, Takeshi Yagi, Takeshi Yada, and Shigeki Goto.<br />
+					Proceedings of the International Conference on Security and Privacy in Communication Networks (SecureComm), October 2016.<br />
+					Acceptance rate: 23.4% (32/137)</li>
+				<li><b><a href="https://doi.org/10.1109/COMPSAC.2015.76" target="_blank">MineSpider : Extracting URLs from Environment-dependent Drive-by Download Attacks.</a></b> <a href="https://doi.org/10.1109/COMPSAC.2015.76" target="_blank"><span class="label label-info">DOI</span></a><br />
+					<span class="me">Yuta Takata</span>, Mitsuaki Akiyama, Takeshi Yagi, Takeo Hariu, and Shigeki Goto.<br />
+					Proceedings of the 39th Annual International Computers, Software & Applications Conference (COMPSAC), July 2015.</li>
+				<li><b><a href="https://doi.org/10.7125/APAN.32.7" target="_blank">Analysis of Redirection Caused by Web-based Malware.</a></b> <a href="https://doi.org/10.7125/APAN.32.7" target="_blank"><span class="label label-info">DOI</span></a>&nbsp;<span class="label label-important">Best Student Paper Award</span><br />
+					<span class="me">Yuta Takata</span>, Shigeki Goto, and Tatsuya Mori.<br />
+					Proceedings of the Asia Pacific Advanced Network (APAN) 2011, August 2011.
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="row">
+		<div class="span12">
+			<h3>Poster</h3>
+			<ul>
+				<li><b><a href="https://doi.org/10.1145/3184558.3186903" target="_blank">POSTER: Predicting Website Abuse Using Update Histories.</a></b> <a href="https://doi.org/10.1145/3184558.3186903" target="_blank"><span class="label label-info">DOI</span></a><br />
+					<span class="me">Yuta Takata</span>, Mitsuaki Akiyama, Takeshi Yagi, Kunio Hato, and Shigeki Goto<br />
+					In WWW'18 Companion: The 2018 Web Conference Companion, April 2018.</li>
+				<li><b><a href="https://www.internetsociety.org/resources/doc/2017/protecting-android-apps-from-repackaging-by-self-protection-code/" target="_blank">POSTER: Protecting Android Apps from Repackaging by Self-Protection Code.</a></b> <a href="https://www.internetsociety.org/resources/doc/2017/protecting-android-apps-from-repackaging-by-self-protection-code/" target="_blank"><span class="label label-info">Poster</span></a><br />
+					Fumihiro Kanei, <span class="me">Yuta Takata</span>, Mitsuaki Akiyama, Takeshi Yagi, and Takeshi Yada<br />
+					In the Network and Distributed System Security Symposium (NDSS), Feb 2017.</li>
+				<li><b>POSTER: Canvas Fingerprinting in the Wild: A Large-scale Measurement and Evaluation.</b><br />
+					Yumehisa Haga, <span class="me">Yuta Takata</span>, Mitsuaki Akiyama, Tatsuya Mori, and Shigeki Goto.<br />
+					In the 18th International Symposium on Research in Attacks, Intrusions and Defenses (RAID), Nov 2015.</li>
+				<li><b>POSTER: Observing Interaction between Java and JavaScript for privacy leakage detection in Android.</b><br />
+					Fumihiro Kanei, Mitsuaki Akiyama, <span class="me">Yuta Takata</span>, and Takeshi Yada.<br />
+					In the 18th International Symposium on Research in Attacks, Intrusions and Defenses (RAID), Nov 2015.</li>
+				<li><b><a href="https://doi.org/10.1145/2810103.2810112" target="_blank">POSTER: Detecting Malicious Web Pages based on Structural Similarity of Redirection Chains.</a></b> <a href="https://doi.org/10.1145/2810103.2810112" target="_blank"><span class="label label-info">DOI</span></a><br />
+					Toshiki Shibahara, Takeshi Yagi, Mitsuaki Akiyama, <span class="me">Yuta Takata</span>, and Takeshi Yada.<br />
+					In the 22nd ACM Conference on Computer and Communications Security (CCS), Oct 2015.</li>
+				<li><b>POSTER: Extracting Redirect-Chain Variations in Drive-by Download Attacks Using Emulati on of Various Client Environments.</b><br />
+					<span class="me">Yuta Takata</span>, Mitsuaki Akiyama, and Takeo Hariu.<br />
+					In the 23rd USENIX Security Symposium, Aug 2014.</li>
+			</ul>
+		</div>
+	</div>
+	<div class="row">
+		<div class="span12">
+			<h3>Book</h3>
+			<ul>
+				<li><a href="https://amzn.to/2Bmf5WZ" target="_blank"><img border="0" src="//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=4339028533&Format=_SL110_&ID=AsinImage&MarketPlace=JP&ServiceVersion=20070822&WS=1&tag=jp1125-22" /></a>&nbsp;
+				<b>実践サイバーセキュリティモニタリング <a href="https://amzn.to/2Bmf5WZ" target="_blank"><span class="label label-info">Amazon</span></a></b><br />
+				八木 毅, 青木 一史, 秋山 満昭, 幾世 知範, <span class="me">高田 雄太</span>, 千葉 大紀<br />
+				コロナ社, 2016年3月.</li>
+			</ul>
+		</div>
+	</div>
+	<div class="row">
+		<div class="span12">
+			<h3>Misc</h3>
+			<ul>
+				<li><b>A Study on the Vulnerabilities of Mobile Apps associated with Software Modules.</b> <a href="https://arxiv.org/abs/1702.03112" target="_blank"><span class="label label-info">preprint</span></a><br />
+					Takuya Watanabe, Mitsuaki Akiyama, Fumihiro Kanei, Eitaro Shioji, <span class="me">Yuta Takata</span>, Bo Sun, Yuta Ishi, Toshiki Shibahara, Takeshi Yagi, and Tatsuya Mori.<br />
+					arXiv:1702.03112</li>
+				<li><b>Security Intelligence for Malware Countermeasures to Support NTT Group's Security Business.</b> <a href="https://www.ntt-review.jp/archive/ntttechnical.php?contents=ntr201512fa3.pdf" target="_blank"><span class="label label-info">PDF (en)</span></a>
+ <a href="http://www.ntt.co.jp/journal/1510/files/jn201510018.pdf" target="_blank"><span class="label label-info">PDF (ja)</span></a><br />
+				Takeo Hariu, Keiichi Yokoyama, Mitsuhiro Hatada, Tekashi Yada, Takeshi Yagi, Mitsuaki Akiyama, Tomonori Ikuse, <span class="me">Yuta Takata</span>, Daiki Chiba, and Yasuyuki Tanaka.<br />
+				NTT Technical Review, Vol.13, No.12, 2015.</li>
+				<li>早稲田大学 非常勤講師 (2018)</li>
+				<li><a href="http://www.iwsec.org/mws/" target="_blank">マルウェア対策研究人材育成ワークショップ (MWS)</a>: 実行委員長 (<a href="http://www.iwsec.org/mws/2018/" target="_blank">2018</a>), 企画委員 (<a href="http://www.iwsec.org/mws/2017/" target="_blank">2017</a>, <a href="http://www.iwsec.org/mws/2016/" target="_blank">2016</a>, <a href="http://www.iwsec.org/mws/2015/" target="_blank">2015</a>), プログラム委員 (<a href="http://www.iwsec.org/mws/2014/" target="_blank">2014</a>)</li>
+				<li><a href="http://www.iwsec.org/css/" target="_blank">コンピュータセキュリティシンポジウム (CSS)</a>: 実行委員 (<a href="http://www.iwsec.org/css/2018/" target="_blank">2018</a>)</li>
+				<li>日本電信電話株式会社 (NTT) 社長表彰: 業務成績表彰(2017), 発明考案表彰(2015)</li>
+				<li>セキュリティ競技 MWS Cup: 芸術部門優勝(2013), 総合優勝(2012), 芸術部門優勝(2010)</li>
+			</ul>
+		</div>
+	</div>
+	<hr />
+</div> <!-- /container -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap-dropdown.js"></script>
+<script src="js/bootstrap-collapse.js"></script>
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-22807983-1']);
+_gaq.push(['_trackPageview', '/index-ja']);
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+$(function(){
+	$("a").bind("click", function(){
+		var data = $(this).attr("href");
+		_gaq.push(['_trackEvent','transfer','click_link',data]);
+	});
+});
+</script>
+</body>
+</html>
